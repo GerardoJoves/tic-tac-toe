acd15935d148d071c1fd8e6984c6f7b064e47f3e
Gerardo Joves <gerardooenrrique@gmail.com>
2022-07-18 16:35:43 -0400

    bind DOM elements to events for allowing players to interact with the board, and add logic to change turns each time a mark is added

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const gameBoard = (function() {
         return board;
     }
     function addMark(index, mark) {
-        if(board[index] === undefined) board[index] = mark;
+        if(board[index] === undefined) {
+            board[index] = mark;
+            return true;
+        } else return false;
     }
     function resetBoard() {
         board = [];
@@ -19,18 +22,34 @@ const gameBoard = (function() {
 })();
 
 const displayController = (function() {
+    let players;
+    let currentPlayer;
     //cacheDOM 
     const squareElements = Array.from(document.querySelectorAll('.square'));
+    //bindEvents
+    squareElements.forEach((square, i) => square.addEventListener('click', () => drawMark(i, currentPlayer.mark)))
 
     function render(board) {
         squareElements.forEach((square, i) => square.textContent = board[i] ?? '');
     }
-    function drawMark(index, mark) {
-        gameBoard.addMark(index, mark);
-        render(gameBoard.getBoard());
+    function drawMark(index, mark) { 
+        if(gameBoard.addMark(index, mark)) {
+            render(gameBoard.getBoard());
+            changeTurn();
+        }
+    }
+    function setPlayers(player1, player2) {
+        players = [player1, player2];
+        currentPlayer = players[0];
+    }
+    function changeTurn() {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
     }
 
     return {
         drawMark,
+        setPlayers,
     }
 })();
+
+displayController.setPlayers({mark: 'X'}, {mark: 'O'})
